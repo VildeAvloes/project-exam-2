5d34e37a64e51afa74df1a3af498cba892bf5091
Vilde Avløs <vildeavlos@gmail.com>
2026-02-26 12:09:45 +0100

    set up prettier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="container py-4">
       <h1 className="display-6">Home</h1>
+      <h2 className="logo">Holidaze</h2>
       <button className="btn btn-primary">Bootstrap funker</button>
     </div>
   );
