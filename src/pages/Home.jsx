c543dab62fd2a223b19c356287f156f33d74803f
Vilde Avløs <vildeavlos@gmail.com>
2026-02-26 11:07:07 +0100

    Installed bootstrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="container py-4">
+      <h1 className="display-6">Home</h1>
+      <button className="btn btn-primary">Bootstrap funker</button>
+    </div>
+  );
 }
