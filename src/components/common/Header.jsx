2513fe10cd61e6062b62cc605256a8909719ee69
Vilde Avløs <vilde.avlos@gyldendal.no>
2026-03-21 17:33:47 +0100

    working on header component and added google fonts and icons

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,35 +1,49 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleCloseMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <header>
-      <nav className="navbar navbar-expand-lg bg-white">
+      <nav className="navbar navbar-expand-lg bg-light">
         <div className="container">
-          <Link className="navbar-brand logo" to="/">
+          <Link className="navbar-brand logo" to="/" onClick={handleCloseMenu}>
             Holidaze
           </Link>
 
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#mainNavbar"
+            onClick={handleCloseMenu}
+            aria-label="Toggle navigation"
+            aria-expanded={isOpen}
+            aria-controls="mainNavbar"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="material-symbols-outlined hamburger">
+              {isOpen ? "close" : "menu"}
+            </span>
           </button>
 
           <div
-            className="collapse navbar-collapse justify-content-center"
+            className={`collapse navbar-collapse justify-content-center ${isOpen ? "show" : ""}`}
             id="mainNavbar"
           >
-            <div className="navbar-nav">
-              <Link className="nav-link" to="/">
+            <div className="navbar-nav mx-lg-auto text-start text-lg-center">
+              <Link className="nav-link" to="/" onClick={handleCloseMenu}>
                 Home
               </Link>
-              <Link className="nav-link" to="/contact">
+              <Link
+                className="nav-link"
+                to="/contact"
+                onClick={handleCloseMenu}
+              >
                 Contact
               </Link>
-              <Link className="nav-link" to="/login">
+              <Link className="nav-link" to="/login" onClick={handleCloseMenu}>
                 Login
               </Link>
             </div>
@@ -37,7 +51,7 @@ export default function Header() {
         </div>
       </nav>
 
-      <div className="bg-white pb-3">
+      <div className="bg-light py-3">
         <div className="container">
           <form className="d-flex justify-content-center">
             <div className="d-flex w-100 w-lg-50 gap-2">
@@ -46,7 +60,9 @@ export default function Header() {
                 type="search"
                 placeholder="Search venues"
               />
-              <button className="btn btn-primary">Search</button>
+              <button className="btn btn-primary btn-icon">
+                <span className="material-symbols-outlined">search</span>
+              </button>
             </div>
           </form>
         </div>
